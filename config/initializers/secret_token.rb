28b07750ed5b0eefb338e083a01a2a7438fa776d
Craig Tamlin <craig.tamlin@gmail.com>
2014-01-02 10:33:44 +0800

    Initialise SimpleApp repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SimpleApp::Application.config.secret_token = '04bf0afab3713fbf1988f29509d801a6e64c7ab47d3fb0850f4a1d5d8409b8dc13bb22b461fbfca72ebf9d610d9e2c9e2698d20a5ef84e715e5d771659a2f9e8'
